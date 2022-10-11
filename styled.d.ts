7472415fe851b242a3e5b75cdc1f73e9e4b80d35
Collins Oriakhi <collins.oriakhi@itara.co>
2022-10-11 10:38:36 +0100

    Completed Authentication with Google

--- styled.d.ts
+++ styled.d.ts
@@ -1,10 +1,12 @@
-import 'styled-components'
+import "styled-components";
 
-declare module 'styled-components' {
+declare module "styled-components" {
   export interface DefaultTheme {
-    
-      primary: string
-      secondary: string
-    
+    primary: string;
+    secondary: string;
+    white: string;
+    brightBlue: string;
+    silver: string;
+    darkGrey: string;
   }
 }
