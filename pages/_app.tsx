74fed70c50e6fbf6cad6d16915a6c0154ac13f2b
Collins Oriakhi <collins.oriakhi@itara.co>
2022-10-11 09:44:02 +0100

    Configured styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import GlobalStyle from '../globalstyles'
 
 const theme: DefaultTheme = {
-  colors: {
+  // colors: {
     primary: '#111',
     secondary: '#0070f3',
-  },
+  // },
 }
 
 export default function App({ Component, pageProps }: AppProps) {
--- styled.d.ts
+++ styled.d.ts
@@ -2,9 +2,9 @@ import 'styled-components'
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {
+    
       primary: string
       secondary: string
-    }
+    
   }
 }
